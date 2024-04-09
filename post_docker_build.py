c7c5c6f7940b9ec2bfab7ae049a7777363b7e39c
WEN Hao <wenh06@gmail.com>
2024-04-09 15:31:13 +0800

    update post docker build script

--- post_docker_build.py
+++ post_docker_build.py
@@ -7,21 +7,10 @@ from cfg import ModelCfg
 from const import DATA_CACHE_DIR, MODEL_CACHE_DIR, REMOTE_HEADS_URLS
 from data_reader import CINC2024Reader
 from models import MultiHead_CINC2024
+from utils.ecg_image_generator.HandwrittenText import download_en_core_sci_sm
 from utils.misc import url_is_reachable
 
 os.environ["HF_HOME"] = str(MODEL_CACHE_DIR)
-
-
-def cache_data():
-    """Cache the synthetic image data and the subset."""
-    print("Caching the synthetic image data...")
-    reader_kwargs = {
-        "db_dir": Path(DATA_CACHE_DIR),
-        "synthetic_images_dir": Path(DATA_CACHE_DIR) / "synthetic_images",
-    }
-    dr = CINC2024Reader(**reader_kwargs)
-    dr.download_synthetic_images(set_name="subset")  # "full" is too large, not uploaded to any cloud storage
-    dr.download_subset()
 
 
 def cache_pretrained_models():
@@ -38,9 +27,40 @@ def cache_pretrained_models():
         model_dir=model_dir,
     )
 
+    # Download the spacy model
+    download_en_core_sci_sm()
+
+
+def cache_data():
+    """Cache the synthetic image data and the subset."""
+    print("Caching the synthetic image data...")
+    reader_kwargs = {
+        "db_dir": Path(DATA_CACHE_DIR),
+        "synthetic_images_dir": Path(DATA_CACHE_DIR) / "synthetic_images",
+    }
+    dr = CINC2024Reader(**reader_kwargs)
+    dr.download_synthetic_images(set_name="subset")  # "full" is too large, not uploaded to any cloud storage
+    dr.download_subset()
+
+
+def prepare_synthetic_images():
+    """Prepare the synthetic images."""
+    print("Preparing the synthetic images...")
+    reader_kwargs = {
+        "db_dir": Path(DATA_CACHE_DIR),
+        "synthetic_images_dir": Path(DATA_CACHE_DIR) / "synthetic_images",
+    }
+    dr = CINC2024Reader(**reader_kwargs)
+    config_index = 0  # -1 for default config, taking no action
+    if config_index >= 0:
+        config = dict(**dr.__gen_img_extra_configs__[config_index])
+    else:
+        config = dict()
+    dr.prepare_synthetic_images(parallel=True, **config)
+
 
 if __name__ == "__main__":
     transformers.logging.set_verbosity_info()
-    cache_data()
     cache_pretrained_models()
+    cache_data()  # or prepare_synthetic_images(), prepare_synthetic_images NOT tested yet.
     print("Done.")
