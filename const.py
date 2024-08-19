d30b8c8ca00d3db6a8cd626d8cb477e44a3dae51
WEN Hao <wenh06@gmail.com>
2024-08-19 10:38:54 +0800

    remove unnecessary constants

--- const.py
+++ const.py
@@ -15,7 +15,6 @@ __all__ = [
     "DATA_CACHE_DIR",
     "TEST_DATA_CACHE_DIR",
     "FULL_DATA_CACHE_DIR",
-    "REMOTE_HEADS_URLS",
     "REMOTE_MODELS",
 ]
 
@@ -52,54 +51,6 @@ DATA_CACHE_DIR = str(
 Path(DATA_CACHE_DIR).mkdir(parents=True, exist_ok=True)
 TEST_DATA_CACHE_DIR = str(Path(DATA_CACHE_DIR) / "cinc2024_test_data")
 FULL_DATA_CACHE_DIR = str(Path(DATA_CACHE_DIR) / "cinc2024_full_data")
-
-
-REMOTE_HEADS_URLS = {
-    "hf--facebook/convnextv2-large-22k-384": {
-        "dropbox": (
-            "https://www.dropbox.com/scl/fi/c38dgecawfy7rhg1gjjgq/"
-            "hf-facebook-convnextv2-large-22k-384-dx-headonly4_04-04_07-32_epochloss_202.66414_metric_0.78.pth.tar"
-            "?rlkey=7za4y2o7ayarjuyyi4dawjrcq&dl=1"
-        ),
-        "deep-psp": (
-            "https://deep-psp.tech/Models/CinC2024/"
-            "hf-facebook-convnextv2-large-22k-384-dx-headonly4_04-04_07-32_epochloss_202.66414_metric_0.78.pth.tar"
-        ),
-    },
-    "hf--facebook/convnextv2-atto-1k-224": {
-        "dropbox": (
-            "https://www.dropbox.com/scl/fi/u62mdypjculkgrkm4skj2/"
-            "hf-facebook-convnextv2-atto-1k-224-dx-headonly__15_04-05_07-06_epochloss_224.66350_metric_0.76.pth.tar"
-            "?rlkey=iotmbhnnzwicf14b5cpjiymda&dl=1"
-        ),
-        "deep-psp": (
-            "https://deep-psp.tech/Models/CinC2024/"
-            "hf-facebook-convnextv2-atto-1k-224-dx-headonly__15_04-05_07-06_epochloss_224.66350_metric_0.76.pth.tar"
-        ),
-    },
-    "hf--facebook/convnextv2-nano-22k-384": {
-        "dropbox": (
-            "https://www.dropbox.com/scl/fi/0z4gcs4acyd39mjzkcuj2/"
-            "hf-facebook-convnextv2-nano-22k-384-dx-headonly__3_04-05_15-49_epochloss_296.05308_metric_0.79.pth.tar"
-            "?rlkey=afir38rv2s4fghfb4l24jzjg4&dl=1"
-        ),
-        "deep-psp": (
-            "https://deep-psp.tech/Models/CinC2024/"
-            "hf-facebook-convnextv2-nano-22k-384-dx-headonly__3_04-05_15-49_epochloss_296.05308_metric_0.79.pth.tar"
-        ),
-    },
-    "hf--microsoft/swinv2-base-patch4-window12to24-192to384-22kto1k-ft": {
-        "dropbox": (
-            "https://www.dropbox.com/scl/fi/876002r87cm756mvaweca/"
-            "hf-microsoft-swinv2-base-patch4-window12to24-192to384-22kto1k-ft-dx-headonly__1_04-05_11-30_epochloss_395.73687_metric_0.75.pth.tar"
-            "?rlkey=pvc4vekswnmuldj3syopck9r8&dl=1"
-        ),
-        "deep-psp": (
-            "https://deep-psp.tech/Models/CinC2024/"
-            "hf-microsoft-swinv2-base-patch4-window12to24-192to384-22kto1k-ft-dx-headonly__1_04-05_11-30_epochloss_395.73687_metric_0.75.pth.tar"
-        ),
-    },
-}
 
 
 REMOTE_MODELS = {
